527f28553a35df89ed99385b486c7333ff190b2a
FloodHydrology <cnjones7@ua.edu>
2019-10-17 11:46:57 -0500

    Kinematic Wave Routing

--- 1_Initial_Toy_Model.R
+++ 1_Initial_Toy_Model.R
@@ -5,14 +5,10 @@
 #Purpose: Begin exploring Toy Model to explore drying and its impact on Biogeochemistry
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-#Potential Workflow
-  #-10 reaches w/ user specified hydraulic geometry 
-  #-each reach has a no-flow threshold 
-  #-when reach volume > no-flow threshold, water is routed using Muskigum-Cunge method 
-  #-when reach volume < no-flow threshold, water "infiltrates" at user specified rate 
-  #-I'm trying to implement "flux-tracking" to estimate residence time. We'll see if that happens by Thursday.
-  #-For biogeochemistry: looking for input! 
- 
+#Next Steps 
+# -Add 
+
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #1.0 Setup Workspace------------------------------------------------------------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -23,17 +19,18 @@ rm(list=ls())
 library(tidyverse)
 
 #User defined variables
-reach_number <- 10     # 10 reaches
-reach_length <- 100    # Each reach is 100 m in length
-reach_slope <- 0.001   # Slope of each reach 
-reach_bkf_depth <- 0.5 # Bankful depth
-reach_wd_ratio <- 2    # Width to depth ratio of bankful channel
-reach_noflow <-0.05    # No flow threshold (as a proportion of bankful flow)
-reach_roughness<-0.03  # Mannings N
+reach_length <- 10000       # Each reach is 10 km in length
+reach_slope <- 0.0003       # Slope of each reach 
+reach_bkf_depth <- 1    # Bankful depth
+reach_wd_ratio <- 5       # Width to depth ratio of bankful channel
+reach_roughness<-0.07      # Mannings N
+simulation_length<-2       # days
+
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#2.0 Estimate Bankfull Flow (Mannings)------------------------------------------
+#2.0 Estimate hydrologic parameters---------------------------------------------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#2.1 Hydraulic Geometry---------------------------------------------------------
 #Estimate bankful width (assume rectangular channel)
 reach_bkf_width<-reach_wd_ratio*reach_bkf_depth
 
@@ -41,55 +38,69 @@ reach_bkf_width<-reach_wd_ratio*reach_bkf_depth
 Q_bkf<- 
   (1/reach_roughness)*                             #k/n
   (reach_bkf_depth*reach_bkf_width)*               #Bankful Area
-  ((reach_bkf_depth*2+reach_bkf_width)^(2/3))*     #Hydraulic Radius ^ (2/3)
+  (reach_bkf_width^(2/3))*     #Hydraulic Radius ^ (2/3)
   (reach_slope^.5)                                 #slope^0.5
 
 #Estimate Bankful Velocity 
 v_bkf<- Q_bkf/(reach_bkf_depth*reach_bkf_width)
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#3.0 Create hydrograph----------------------------------------------------------
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#2.1 Inflow hydrograph ---------------------------------------------------------
+#Define time step so that the Courant number (v*dt/dx) is less than or equal to 1
+timestep<- floor((reach_length/v_bkf))
+
 #Create tibble
 df<-tibble(
-  #Create 28 day period
-  time = seq(0,(24*28)),
-  #Create flow 
-  Q_upstream = c(0,rep(c(rep(Q_bkf, 24), rep(0, (24*6))), 4))
-)
+      #Create 2 time steps
+      time = seq(0,simulation_length*86400, timestep),
+      #Create flow 
+      Q_in = Q_bkf*sin(pi*time/86400) + Q_bkf/10
+    ) %>% 
+  mutate(Q_in = if_else(Q_in<=Q_bkf/10, Q_bkf/10, Q_in))
+
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#4.0 Create function to estimate outflow [Kinematic Wave Model]-----------------
+#4.0 Create function to estimate fluxes-----------------------------------------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-mk_routing<-function(Q_in,   #Inflow
-                     Q_n,    #Qout from previous time step
-                     v_bkf,  #Bankful velocity 
-                     w_bkf,  #Bankful widt
-                     slope,  #channel slope
-                     dt,     #time step
-                     dx,     #chanel length
-                     threshold = reach_noflow #no flow threshold
-                     ){
-
-  #Estimate dynamic Muskigum-Cunge variables
-  c <- (5/3)*v_bkf
-  k <- dx/c
-  q0<- Q_bkf/w_bkf
-  x <- 0.5*(1-(q0/slope/c/dx))
+#4.1 Kinematic Wave-------------------------------------------------------------
+kinematic_wave<-function(Q_in,                    #Inflow
+                         Q_in_1,                  #Qout from previous time step
+                         dt = timestep,           #time step (seconds)
+                         dx = reach_length,       #Reach length
+                         slope =  reach_slope,    #Channel Slope
+                         width = reach_bkf_width, #Channel Width
+                         n = reach_roughness      #mannings roughness
+                        ){
+  #Estiate model parameters
+  beta <- 3/5  #Common Assumption (See Chow 1988)
+  alpha<- (n*(width^(2/3))/(slope^0.5))^beta
   
-  #Estimate Muskigum-Cunge Cofficients
-  C_0 <- ((-1*k*x)+(0.5*dt))/((k*(1-x))+(0.5*dt))
-  C_1 <- ((k*x)+(0.5*dt))/((k*(1-x))+(0.5*dt))
-  C_2 <- ((k*(1-x))-(0.5*dt))/((k*(1-x))+(0.5*dt))
+  #Estimate Reach Outflow
+  Q_out<- Q_in + (((Q_in^(1-beta))/(alpha*beta*dx))*(Q_in_1-Q_in))*dt
   
-  #Estimate Q_out
-  Q_out<-C_0*Q_in+C_1*Q_in+C_2*Q_n
-    
-  #Test to see if Q_out<threshold
-  Q_out<-if_else(Q_out>threshold, Q_out, 0)
-    
-  #Export Q_out 
+  #Export Qout
+  #Q_out<-if_else(Q_out<0, 0, Q_out)
   Q_out
 }
 
+#Conservative Solute Flux-------------------------------------------------------
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#5.0 Create function to estimate conservative solute flux-----------------------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+df<-df %>%
+  #Run model
+  mutate(Q_out = 
+     pmap_dbl(
+       #Model Inputs
+       list(Q_in =   Q_in, 
+            Q_in_1 = lag(Q_in, default = 0)), 
+       #Function
+       kinematic_wave)) %>% 
+  #Add time_hr 
+  mutate(time_hr = time/3600)
+
+plot(df$time_hr, df$Q_in, type="l")
+points(df$time_hr, df$Q_out, col="red", type="l")
 
